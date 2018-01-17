d18b029790323dfd288c99a4849f0c9f928023aa
Fabio Kfouri <kfouri.fabio@gmail.com>
2018-01-16 23:32:10 -0200

    estudo do tkinter. Nao deu certo ainda o teste

---
+++ Udemy/UI/tkinter.py
@@ -0,0 +1,19 @@
+'''
+Este codigo nao consegui funcionar. Estudar novamente
+https://www.udemy.com/algoritmos-e-estruturas-de-dados-com-python3/learn/v4/t/lecture/4305204?start=0
+'''
+import sys
+if(sys.version_info[0]<3):
+    from Tkinter import *
+else:
+    from tkinter import *
+#import tkinter as tk
+
+#cria uma nova Janela
+window = Tk()
+
+#titulo da janela
+window.title('Primeira Janela')
+
+#desenhar a janela e iniciar a aplicacao
+window.mainloop()
