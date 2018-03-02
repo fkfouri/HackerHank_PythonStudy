d20bb0924820191778155e116de06fed1dd3ca3c
Fabio Kfouri <kfouri.fabio@gmail.com>
2018-03-01 23:48:30 -0300

    Estudo sobre obtencao de dados na WEB

---
+++ Udemy/library and functions/GetDataFromWeb/libs.py
@@ -0,0 +1,11 @@
+'''
+Send HttpRequest and Get HttpResponse
+- requests
+- urlib.requests
+
+Parse the response and Extract Data
+- json
+- lxml
+- BeautifulSoup, Selenium (for html data)
+
+'''
---
+++ Udemy/library and functions/GetDataFromWeb/requests.py
@@ -0,0 +1,13 @@
+'''
+Exemplo usando a biblioteca requests
+'''
+import requests
+
+
+url = "https://en.wikipedia.org/wiki/main_page"
+#The rest of your code should go below this line
+r = requests.get(url)
+print(r.status_code)
+wikitext = r.content.decode('utf-8')
+
+wikitext.find('Did you know')
