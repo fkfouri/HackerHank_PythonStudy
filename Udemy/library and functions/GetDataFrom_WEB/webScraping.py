d4b93a4d047a946bdb91b97d623859c6ea502f47
Fabio Kfouri <kfouri.fabio@gmail.com>
2018-03-05 22:25:45 -0300

    Estudos da quarta semana

---
+++ Udemy/library and functions/GetDataFrom_WEB/webScraping.py
@@ -0,0 +1,7 @@
+'''
+Basicamente ha tres bibliotecas
+- requests: http request e response
+- Beautiful Soup: utiliza a tag estruturada html para parsear o conteudo
+- Selenium: emula o browser.  Util quando a pagina tem scripts
+'''
+
